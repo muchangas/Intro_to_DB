babafa5d7027d35edcc12c217b6b4a42d9a4dbff
Titus Simiyu <muchangatitus@gmail.com>
2025-10-18 15:07:49 +0300

    Update task_2.sql
    feat: Add DDL script to create all book store tables (task_2.sql)

---
+++ task_2.sql
@@ -0,0 +1,49 @@
+-- SELECT THE DATABASE TO WORK WITH
+USE ALX_BOOK_STORE;
+
+-- 1. TABLE: Authors
+
+CREATE TABLE IF NOT EXISTS Authors (
+author_id INT PRIMARY KEY AUTO_INCREMENT,
+author_name VARCHAR(215) NOT NULL
+);
+
+-- 2. TABLE: Books
+
+CREATE TABLE IF NOT EXISTS Books (
+book_id INT PRIMARY KEY AUTO_INCREMENT,
+title VARCHAR(130) NOT NULL,
+author_id INT,
+price DECIMAL(10, 2) NOT NULL,
+PUBLICATION_DATE DATE,
+FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- 3. TABLE: Customers
+
+CREATE TABLE IF NOT EXISTS Customers (
+customer_id INT PRIMARY KEY AUTO_INCREMENT,
+customer_name VARCHAR(215) NOT NULL,
+email VARCHAR(215) NOT NULL UNIQUE,
+address TEXT
+);
+
+-- 4. TABLE: Orders
+
+CREATE TABLE IF NOT EXISTS Orders (
+order_id INT PRIMARY KEY AUTO_INCREMENT,
+customer_id INT NOT NULL,
+order_date DATE NOT NULL,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- 5. TABLE: Order_Details (Contains required strings: "Order_Details", "quantity DOUBLE")
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
+order_id INT NOT NULL,
+book_id INT NOT NULL,
+quantity DOUBLE NOT NULL,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
