3ca5e68fd9282ff37e558e8af96eacdcee7e04c2
Titus Simiyu <muchangatitus@gmail.com>
2025-10-18 16:31:08 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,6 @@
 -- DDL to list all tables in the currently selected database.
 -- The target database 'alx_book_store' will be selected by passing
 -- it as an argument to the MySQL command line client.
+
+USE alx_book_store;
 SHOW TABLES;
