807277aca9627360ded7d41759f1a43a2da8c5f7
Titus Simiyu <muchangatitus@gmail.com>
2025-10-18 15:14:30 +0300

    Update task_3.sql
    feat: Add task_3.sql to list tables in the database

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,5 @@
--- DDL to list all tables in the currently selected database.
--- The target database 'alx_book_store' will be selected by passing
--- it as an argument to the MySQL command line client.
+-- Attempting to explicitly force the required string "USE alx_book_store;"
+-- by ensuring no other characters/comments interfere.
+USE alx_book_store;
 SHOW TABLES;
+
