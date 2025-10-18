a48e25b946ce85ccf8a87cfdfaff3de48d480365
Titus Simiyu <muchangatitus@gmail.com>
2025-10-18 16:22:40 +0300

    feat: Add task_5.sql to insert first customer row

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,4 @@
--- Attempting to explicitly force the required string "USE alx_book_store;"
--- by ensuring no other characters/comments interfere.
-USE alx_book_store;
+-- DDL to list all tables in the currently selected database.
+-- The target database 'alx_book_store' will be selected by passing
+-- it as an argument to the MySQL command line client.
 SHOW TABLES;
-
